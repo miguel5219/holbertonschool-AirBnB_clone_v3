3b92bc180e6bdd033c4039ee3f0e05e90bf1b0d8
miguel5219 <5219@holbertonstudents.com>
2022-12-15 16:06:30 -0500

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,10 +10,12 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(exception):
     """ close storage """
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found(error):
