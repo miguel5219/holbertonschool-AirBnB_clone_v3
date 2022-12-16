51b75d0f92e3ca6cc8c05967ca4045ed3ab8babf
miguel5219 <5219@holbertonstudents.com>
2022-12-16 16:02:07 -0500

    update

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -57,3 +57,21 @@ def cities_posts(state_id):
     setattr(city_new, 'state_id', state_id)
     city_new.save()
     return jsonify(city_new.to_dict()), 201
+
+@app_views.route('/cities/<city_id>', strict_slashes=False, methods=['PUT'])
+def put_cities(city_id):
+    """ update state """
+    city_ = storage.get(City, city_id)
+    if not city_:
+        abort(404)
+
+    if not request.get_json():
+        abort(400, "Not a JSON")
+
+    for k, value in request.get_json().items():
+        if k in ["id", "state_id", "created_at", "updated_at"]:
+            continue
+        else:
+            setattr(city_, k, value)
+    storage.save()
+    return jsonify(city_.to_dict(), 200)
